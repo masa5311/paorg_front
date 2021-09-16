0a7cc9533055ca421e94ce4dd47583361cccae47
masa <masa.docomo.ne.jp@y7.dion.ne.jp>
2021-09-16 21:38:52 +0900

    FirebaseUIインストール

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    "firebaseui/dist/firebaseui.css"
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
